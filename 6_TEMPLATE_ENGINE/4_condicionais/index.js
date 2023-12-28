573b900c98d56e0fb20080141a68711f35317292
Emerson Thiago <emersonthiago168@outlook.com>
2023-12-28 15:00:09 -0300

    condicionais no Handlebars

---
+++ 6_TEMPLATE_ENGINE/4_condicionais/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: 'Emerson',
+        surname: 'Thiago',
+        age: 20
+    }
+
+    const palavra = 'Teste';
+
+    const auth = true;
+
+    res.render('home', { user, palavra, auth });
+});
+
+app.listen(3000, () => {
+    console.log('App funcionando!');
+})
